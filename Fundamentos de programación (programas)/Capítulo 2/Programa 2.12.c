3d91767b444b1c6ea66409ac495e25d2ec8e9832
Carlos-Almonte08 <157220900+Carlos-Almonte08@users.noreply.github.com>
2024-02-02 07:47:50 -0400

    Proyectos del capitulo 2

---
+++ Fundamentos de programación (programas)/Capítulo 2/Programa 2.12.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <math.h>
+
+/*Igualdad de expresiones.
+El programa, al recibir como datos T, P y N, compruba la igualdad de
+una expersion determinada.
+
+T, P y N: variables de tipo entero. */
+
+void main(void)
+{
+int T, P, N;
+printf("Ingrese los valores de T, P y N: ");
+scanf("%d %d %d", &T, &P, &N);
+if (P != 0)
+{
+    if (pow(T / P, N) == (pow(T, N) / pow(P, N))
+       printf("\nSe ccomprueba la igualdad");
+
+
+    else
+        printf("\nNo se comprueba la igualdad");
+
+}
+
+
+    else
+  printf("\nP tiene que ser diferente");
+}
